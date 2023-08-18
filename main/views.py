b14f7499088f9ed3fceb3238738dfc8916e41c7d
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-08-18 18:43:08 +0300

    first commit

---
+++ main/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, PostForm
+from .models import Post
+
+
+@login_required(login_url='/login')
+def home(request):
+    posts = Post.objects.all().select_related('author')
+    context = {'posts': posts}
+    return render(request, 'main/home.html', context=context)
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/sign_up.html', {'form': form})
+
+
+@login_required(login_url='/login')
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('/home')
+    else:
+        form = PostForm()
+    return render(request, 'main/create_post.html', {'form': form})
